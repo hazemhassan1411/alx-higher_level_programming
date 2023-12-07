e6477de4d13010d5d30386b338ad006dd49c1408
hazemhassan1411 <hazemhassan1411@gmail.com>
2023-12-07 14:39:20 +0200

    ss

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(i + 1, 10):
-        print("{:02d}, {:02d}".format(i, j), end=", " if j < 9 else "\n")
+for first_digit in range(10):
+    for second_digit in range(first_digit + 1, 10):
+        if first_digit < 8:
+            print(f"{first_digit}{second_digit}", end=', ')
+        else:
+            print(f"{first_digit}{second_digit}")
