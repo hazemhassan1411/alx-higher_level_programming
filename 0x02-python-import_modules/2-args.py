30c481678101547fd522f9dd33ec595fab5b3ad4
hazemhassan1411 <hazemhassan1411@gmail.com>
2023-12-07 14:16:04 +0200

    SS

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+
+    argc = len(argv) - 1
+    arg_str = "argument" if argc == 1 else "arguments"
+
+    print("{} {}{}:".format(argc, arg_str, '.' if argc == 0 else ''))
+    
+    for i, arg in enumerate(argv[1:], start=1):
+        print("{}: {}".format(i, arg))
