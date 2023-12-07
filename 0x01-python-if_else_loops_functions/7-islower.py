d9fe5bb1a3989dbb253c3f4834d18640065f26b2
hazemhassan1411 <hazemhassan1411@gmail.com>
2023-12-07 12:06:49 +0200

    z

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -2,8 +2,8 @@
 def islower(c):
     return ord('a') <= ord(c) <= ord('z')
 
-print("a is {}".format("lower" if islower("a") else "upper"))
-print("H is {}".format("lower" if islower("H") else "upper"))
-print("A is {}".format("lower" if islower("A") else "upper"))
-print("3 is {}".format("lower" if islower("3") else "upper"))
-print("g is {}".format("lower" if islower("g") else "upper"))
+print("{} => {}".format("a", "lower" if islower("a") else "upper"))
+print("{} => {}".format("H", "lower" if islower("H") else "upper"))
+print("{} => {}".format("A", "lower" if islower("A") else "upper"))
+print("{} => {}".format("3", "lower" if islower("3") else "upper"))
+print("{} => {}".format("g", "lower" if islower("g") else "upper"))
