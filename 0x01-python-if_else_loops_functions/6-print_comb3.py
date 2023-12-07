48f3e7b00cb64f0a1579bb82719f92889b7febd7
hazemhassan1411 <hazemhassan1411@gmail.com>
2023-12-07 12:00:00 +0200

    d

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 for i in range(1, 10):
     for j in range(i + 1, 10):
-        if i == 1 and j == 9:
-            print("{:02d}, {:02d}".format(i - 1, j), end="\n")
-        else:
-            print("{:02d}, {:02d}".format(i - 1, j), end=", ")
+        print("{:02d}, {:02d}".format(i, j), end=", " if i < 8 or j < 9 else "{:02d}, {:02d}\n".format(i, j))
