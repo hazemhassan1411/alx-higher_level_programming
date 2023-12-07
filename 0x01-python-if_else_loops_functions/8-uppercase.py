8bcd700e1f64f4dcc7d57e17edc8230dc64c2ef2
hazemhassan1411 <hazemhassan1411@gmail.com>
2023-12-07 14:48:22 +0200

    ss

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -5,7 +5,6 @@ def uppercase(str):
             char = chr(ord(char) - 32)
         print("{}".format(char), end='')
     print()
-
 if __name__ == "__main__":
     uppercase("best")
     uppercase("Best School 98 Battery street")
