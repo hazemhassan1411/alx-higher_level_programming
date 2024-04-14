f4b04a7a284ec9a49eb6f9b87ba5e237bc84726f
hazemhassan1411 <hazemhassan1411@gmail.com>
2024-04-14 16:28:18 +0200

    s

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-def square_matrix_simple (my_list, replace, search):
+def square_replace (my_list, replace, search):
     return list(map(lambda g: replace if g == search else g, my_list))
