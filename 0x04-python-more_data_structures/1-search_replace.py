12173f6528dedde45e1fdde0d246ba07296978b1
hazemhassan1411 <hazemhassan1411@gmail.com>
2024-04-14 16:29:04 +0200

    s

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-def square_replace (my_list, replace, search):
+def search_replace (my_list, replace, search):
     return list(map(lambda g: replace if g == search else g, my_list))
