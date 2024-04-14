243745849c14c68c103e70eda00df85692ecae55
hazemhassan1411 <hazemhassan1411@gmail.com>
2024-04-14 17:55:51 +0200

    s

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    mv = 0
+    mk = None
+    
+    for k, v in a_dictionary.items():
+        if v > mv:
+            mv = v
+            mk = k    
+        return mk
