794a44681873c4a6bbd7b96b2c729e47fc41141d
hazemhassan1411 <hazemhassan1411@gmail.com>
2024-04-20 17:15:43 +0200

    s

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
 """square class"""
 
+
 class Square:
     """square class"""
 
     def __init__(self, size=0):
-
+        """square class"""
         self.__size = size
 
     @property
     def size(self):
-
+        """square class"""
         return self.__size
 
     @size.setter
@@ -22,11 +23,11 @@ class Square:
         self.__size = value
 
     def area(self):
-    
+        """square class"""
         return  self.__size ** 2
     
     def my_print(self):
-
+        """square class"""
         for i in range(self.__size):
             for j in range(self.__size):
                 print("#", end="\n" if j is self.__size - 1 and i != j else "")
