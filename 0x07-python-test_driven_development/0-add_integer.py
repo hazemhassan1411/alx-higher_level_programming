860b19b116fdbd3c5c859fc768deb896dec9d505
hazemhassan1411 <hazemhassan1411@gmail.com>
2024-04-22 18:32:55 +0200

    a

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -4,12 +4,11 @@
 
 def add_integer(a, b=98):
     """Adds two integers.
-    
-    
-    
+
+
+
     ghg
     """
-
 
     if type(a) not in (int, float):
         raise TypeError('a must be an integer')
@@ -17,6 +16,7 @@ def add_integer(a, b=98):
         raise TypeError('b must be an integer')
     return int(a) + int(b)
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     import doctest
     doctest.testfile("tests/0-add_integer.txt")
