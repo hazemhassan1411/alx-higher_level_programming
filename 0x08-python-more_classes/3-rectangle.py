91feee340190030b5af54d1a197e2b91c00f9bc8
hazemhassan1411 <hazemhassan1411@gmail.com>
2024-05-01 17:46:23 +0300

    s

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -52,3 +52,6 @@ class Rectangle:
         if self.width == 0 or self.height == 0:
             return ""
         return "\n".join(["#" * self.width] * self.height)
+
+    def __repr__(self):
+        return f"Rectangle({self.width}, {self.height})"
