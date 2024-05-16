a3813a7d748f4e7384eb6406285939bfb1e519b7
hazemhassan1411 <hazemhassan1411@gmail.com>
2024-05-16 20:41:31 +0300

    s

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""hazem is here"""
+def read_file(filename=""):
+    """hazem is here"""
+    with open(filename, encoding ='utf-8') as file:
+        print(file.read(), end="")
--- test/quiz.py
+++ test/quiz.py
@@ -1,18 +1,9 @@
-def que():
-    print("what is the beggist planet")
-    print("   A. jupitar")
-    print("   B. earth")
-    print("   C. mercury")
+import os
 
-que()
-AN = input("your answer: ")
+path = "C:\\Users\\hazem\\Desktop\\hazfem"
+
+if os.path.exists:
+    print("this is file is exist")
     
-while not AN == "a":
-    print("_____________________________________________________________")
-    print("try again")
-    print("_____________________________________________________________")
-    que()
-    AN = input("your answer: ")
-
-print("_____________________________________________________________")
-print("right answer")
+else:
+    print("this file dosent exist")
