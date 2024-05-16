82c465552ed819a8b970429d1612ea73eb1836be
hazemhassan1411 <hazemhassan1411@gmail.com>
2024-05-16 20:56:53 +0300

    f

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -2,7 +2,7 @@
 """hazem is here"""
 
 
-def write_file(filename="", text=""):
+def append_write(filename="", text=""):
     """hazem is here"""
     with open(filename, "a", encoding='utf-8') as file:
         return file.write(text)
