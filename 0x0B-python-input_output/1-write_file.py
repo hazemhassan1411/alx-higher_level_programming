6710e144eb3567bab956fbbd27c71cc421b7601e
hazemhassan1411 <hazemhassan1411@gmail.com>
2024-05-16 20:50:18 +0300

    x

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""hazem is here"""
+
+
+def write_file(filename="", text=""):
+    """hazem is here"""
+    with open(filename, 'w', encoding='utf-8') as file:
+        print(file.write(text))
