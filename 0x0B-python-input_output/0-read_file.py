193391bd5cf877849769d5d44b429eb528c2b1e2
hazemhassan1411 <hazemhassan1411@gmail.com>
2024-05-16 20:45:03 +0300

    a

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """hazem is here"""
+
+
 def read_file(filename=""):
     """hazem is here"""
-    with open(filename, encoding ='utf-8') as file:
+    with open(filename, encoding='utf-8') as file:
         print(file.read(), end="")
