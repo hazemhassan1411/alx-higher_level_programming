54a165c3e54c4d2d5d83cc5de09ea52a28b0c1ef
hazemhassan1411 <hazemhassan1411@gmail.com>
2024-06-18 03:50:04 +0300

    r

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,16 +4,23 @@
 import sys
 import os
 
-save_to_json_file = __import__('5-save_to_json_file.py').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file.py').load_from_json_file
+# Import the functions from the previous tasks
+from save_to_json_file import save_to_json_file
+from load_from_json_file import load_from_json_file
 
 filename = "add_item.json"
 
-if os.path.exists(filename):
-    items = load_from_json_file(filename)
-else:
+# Initialize the list
+try:
+    if os.path.exists(filename) and os.path.getsize(filename) > 0:
+        items = load_from_json_file(filename)
+    else:
+        items = []
+except Exception:
     items = []
 
+# Add all arguments to the list
 items.extend(sys.argv[1:])
 
+# Save the list to the file
 save_to_json_file(items, filename)
