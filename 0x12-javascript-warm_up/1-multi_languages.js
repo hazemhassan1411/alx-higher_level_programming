86fcd7b80505cdac3c7c3e997028a084314fc913
hazemhassan1411 <hazemhassan1411@gmail.com>
2024-06-21 23:22:53 +0300

    f

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+/* hiii */
+const fline = 'C is fun';
+const sline = 'Python is cool';
+const tline = 'JavaScript is amazing';
+console.log(fline);
+console.log(sline);
+console.log(tline);
